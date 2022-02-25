fc0cb81946a001d2c29b81b92b921d59cb03fabc
Heine574 <johannes@niemela.se>
2022-02-25 12:54:07 -0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h1 className="unselectable">RPManager</h1>
+      </>
+    );
+  }
+}
+
+export default App;
